8311e49302d76e87301cb13e4fe7ac370e7aa1cd
pethl <pethicklisa@gmail.com>
2025-01-21 13:47:22 +0000

    saved assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,2 @@
 pin "chartkick", to: "chartkick.js"
-pin "Chart.bundle", to: "Chart.bundle.js"
-pin_all_from "app/assets", under: "builds"
+pin "Chart.bundle", to: "Chart.bundle.js"pin "@hotwired/turbo-rails", to: "turbo.min.js"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/views/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/assets/stylesheets/**/*.css",
-    "./app/javascript/**/*.js"
-  ],
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(min-aspect-ratio: 1/2)" }
+        tallscreen: { raw: "(min-aspect-ratio: 13/20)" }
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" }
+        }
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards"
       }
     }
   },
-  plugins: [
-    require("flowbite/plugin")({
-      charts: true
-    })
-  ],
-  content: ["./node_modules/flowbite/**/*.js"],
-  autoprefixer: {}
+  plugins: []
 }
