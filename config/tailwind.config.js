1aa5d6ba798308e12d00519648b1db0d1702cf65
pethl <pethicklisa@gmail.com>
2025-01-21 12:45:53 +0000

    first try at hamburger menu

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,36 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/assets/builds/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}"
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans]
       },
-    },
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 13/20)" }
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" }
+        }
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards"
+      }
+    }
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries")
   ]
 }
