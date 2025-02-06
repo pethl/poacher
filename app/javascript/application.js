06150dcbb85edf7b0c4999f1ab72efbd20684a83
pethl <pethicklisa@gmail.com>
2025-02-06 12:26:22 +0000

    troubleshoot

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,12 +2,3 @@
 import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
 import SignatureController from "./controllers/signature_controller"
-import Chartkick from "chartkick"
-import Chart from "chart.js"
-
-// Add Chart.js as the adapter
-Chartkick.addAdapter(Chart
-
-const application = Application.start()
-console.log("Stimulus is loaded!")
-application.register("signature", SignatureController)
