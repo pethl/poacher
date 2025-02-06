a97acf9175acf5987bcacfd8b0efcf026f2afe32
pethl <pethicklisa@gmail.com>
2025-02-06 12:42:28 +0000

    remove dark

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -104,11 +104,11 @@ module ApplicationHelper
     end
    
    def dropdown_list_class
-     "block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+     "block px-4 py-2 hover:bg-gray-100 "
    end
    
    def table_head_class
-     "text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400"
+     "text-xs text-gray-700 uppercase bg-gray-200 "
    end
    
   
